3458962a7254310be0266e1c30b8e3a7e90aa8ac
Saravanan-dotcom <88078660+Saravanan-dotcom@users.noreply.github.com>
2022-03-24 16:46:02 +0500

    update code

--- javaTask/src/main/java/org/Task/javaTask/App.java
+++ javaTask/src/main/java/org/Task/javaTask/App.java
@@ -1,10 +1,12 @@
 package org.Task.javaTask;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import com.data.StudentData.StudentData;
@@ -20,35 +22,31 @@ public class App
 {
     public static void main( String[] args )
     {
-		// TODO Auto-generated method stub
 		
 		StudentData sd = new StudentData();
-		
-		
-		System.out.println();
-		//List<Student> studentList = sd.getAllStudents();
 		//Consumer
-		Consumer<List<Student> > t1 = s -> {
-			System.out.println("Task1-----------------");
+		Consumer<List<Student>> t1 = s -> {
+			System.out.println("Consumer Get list of Students");
 	            for (int i = 0; i < s.size(); i++)
 	                //list.set(i, 2 * list.get(i));
 	            	System.out.println(s.get(i));
 		};
 		//Supplayer
-		
+		List<Student> studentList = sd.getAllStudents();
+	
 		//predicate
 		Predicate<Student> p = t3 ->  t3.getGradeLevel() > 3;
 		//Function
 		Function<Student, Student> f = s -> s;
 		//predicate
 		Predicate<Student> p2 = t3 ->  t3.getGpa() > 3.5;
-		List<Student> studentList = sd.getAllStudents();
+		
 		t1.accept(studentList);
 		List<Student> collect = studentList.stream().filter(p).map(f).collect(Collectors.toList());
-		System.out.println("Task2-------------------"+collect);
+		System.out.println("Filter the student details based gradelevle-------------------"+collect);
         Map<String, Student> collect2 = studentList.stream().filter(s -> s.getGpa() > 3.5).collect(Collectors.toMap(Student::getName, Function.identity()));
 		
 		
-		System.out.println("Task3-------------------"+collect2);
+		System.out.println("Filter the student details based Gpa-------------------"+collect2);
 		}
 }
